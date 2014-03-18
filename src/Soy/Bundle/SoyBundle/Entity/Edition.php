e0f2f69b925927d6a9714e2407f7d290c341ae8b
vova <vova.ostapiuk@gmail.com>
2014-03-18 21:20:03 +0200

    Entity realized

---
+++ src/Soy/Bundle/SoyBundle/Entity/Edition.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Soy\Bundle\SoyBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Edition
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Edition
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    /**
+     * @ORM\ManyToMany(targetEntity="Card",mappedBy="editions")
+     */
+    private $cards;
+
+    /**
+     * @param mixed $locale
+     */
+    public function setLocale($locale)
+    {
+        $this->locale = $locale;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLocale()
+    {
+        return $this->locale;
+    }
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $locale;
+
+    public function __construct()
+    {
+        $this->cards = new ArrayCollection();
+    }
+
+    /**
+     * @param mixed $cards
+     */
+    public function setCards($cards)
+    {
+        $this->cards = $cards;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCards()
+    {
+        return $this->cards;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function addCard(Card $card)
+    {
+        $this->cards[] = $card;
+    }
+
+    public function removeCard(Card $card)
+    {
+        $this->cards->removeElement($card);
+
+    }
+}
